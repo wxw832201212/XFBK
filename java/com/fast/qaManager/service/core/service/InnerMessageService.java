9d8f56e83a10385ff0b341cbdde98099435b5374
832201124Xiuping Jiang <3369490179@qq.com>
2024-12-20 23:44:30 +0800

    Delete java directory

--- java/com/fast/qaManager/service/core/service/InnerMessageService.java
+++
@@ -1,7 +0,0 @@
-package com.fast.qaManager.service.core.service;
-
-import com.fast.qaManager.service.core.entity.vo.Response;
-
-public interface InnerMessageService {
-    public Response getInnerMessage(String token,Integer type);
-}
--- java/com/fast/qaManager/service/core/service/MessageService.java
+++
@@ -1,8 +0,0 @@
-package com.fast.qaManager.service.core.service;
-
-import com.fast.qaManager.service.core.entity.vo.Response;
-
-public interface MessageService {
-    public Response getMessage(String token,Integer problemId);
-    public Response sendMessage(String token,Integer problemId,String content);
-}
